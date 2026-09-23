2fe512ab35900c282206f0f110d9545fdd27d0fb
JoaquimGSM <joaquimgsm1112@gmail.com>
2026-09-23 16:42:44 -0300

    fix: remove tags HTML das descricoes dos pratos

--- src/components/CardComida.jsx
+++ src/components/CardComida.jsx
@@ -1,11 +1,17 @@
 const CardComida = ({ nome, imagem, descricao }) => {
+
+  const removerHTML = (texto) => {
+    const documento = new DOMParser().parseFromString(texto, "text/html")
+    return documento.body.textContent
+  }
+
   return (
     <div>
       <img src={imagem} alt={nome} />
 
       <h3>{nome}</h3>
 
-      <p>{descricao}</p>
+      <p>{removerHTML(descricao)}</p>
     </div>
   )
 }
