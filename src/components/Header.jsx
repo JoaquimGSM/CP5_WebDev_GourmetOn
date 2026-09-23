2bb0abdab49b3352a6f23c37ea38504a02f08c0b
JoaquimGSM <joaquimgsm1112@gmail.com>
2026-09-23 09:32:54 -0300

    feat: cria componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <a href="#inicio">Início</a>
+        <a href="#sobre">Sobre</a>
+        <a href="#funcionalidades">Funcionalidades</a>
+        <a href="#pratos">Pratos</a>
+        <a href="#depoimentos">Depoimentos</a>
+        <a href="#contato">Contato</a>
+      </nav>
+    </>
+  )
+}
+
+export default Header
