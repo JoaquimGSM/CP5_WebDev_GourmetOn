b9d7c868fdf74c2389b3c352617a972dace63d98
JoaquimGSM <joaquimgsm1112@gmail.com>
2026-09-23 09:42:29 -0300

    feat: cria componente Apresentacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Apresentacao from './components/Apresentacao'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Apresentacao />
     </div>
   )
 }
---
+++ src/components/Apresentacao.jsx
@@ -0,0 +1,17 @@
+
+const Apresentacao = () => {
+  return (
+    <section id="sobre">
+      <h2>Sobre o GourmetOn</h2>
+      
+      <p>O GourmetOn é um aplicativo de delivery criado para facilitar
+        a escolha e o pedido de comidas.</p>
+
+      <p>Com ele, o usuário pode encontrar diferentes opções de pratos,
+        realizar pedidos de forma prática e aproveitar uma experiência
+        rápida e simples.</p>
+    </section>
+  )
+}
+
+export default Apresentacao
