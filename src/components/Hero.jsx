b529b6e3b27748af24df5e1e8161cd26822b1ebc
JoaquimGSM <joaquimgsm1112@gmail.com>
2026-09-23 09:36:05 -0300

    feat: cria componente Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section id="inicio">
+      <h1>Peça sua comida favorita com o GourmetOn</h1>
+      <p>
+        Encontre seus pratos favoritos de forma rápida, fácil e prática.
+      </p>
+      <button>Conheça nossos pratos</button>
+    </section>
+  )
+}
+
+export default Hero
